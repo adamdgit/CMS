4e24256f6e106221a0851baa5ad0b8b7d9bf4ddf
adamdgit <adam_d@outlook.com>
2023-06-29 21:37:30 +1000

    Added register functionality

---
+++ src/routes/register/+page.server.ts
@@ -0,0 +1,46 @@
+import { redirect, fail } from "@sveltejs/kit";
+import type { PageServerData } from "../$types";
+import bcrypt from 'bcrypt'
+import { db } from "$lib";
+import type { Action, Actions } from "@sveltejs/kit";
+
+export const load:PageServerData = async () => {
+  // 
+}
+
+const register:Action = async ({ request }) => {
+  const data = await request.formData()
+  const username = data.get('username')
+  const password = data.get('password')
+  console.log({username, password})
+
+  // simple validation
+  if (typeof username !== 'string' || typeof password !== 'string'
+    || !username || !password) {
+    return fail(400, { user: true })
+  }
+
+  // check if username exists
+  const user = await db.users.findUnique({
+    where: { username }
+  })
+
+  // user already exists
+  if (user) {
+    return fail(400, { user: true })
+  }
+
+  // if no errors, register user
+  await db.users.create({
+    data: {
+      username: username,
+      password: await bcrypt.hash(password, 10),
+      authToken: crypto.randomUUID(),
+      roleId: 2 // 1 = Admin, 2 = User, 3 = Moderator
+    }
+  })
+
+  throw redirect(303, './login')
+}
+
+export const actions: Actions = { register }
---
+++ src/routes/register/+page.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import type { ActionData } from "./$types";
+
+  export let form: ActionData
+</script>
+
+<h1>Register</h1>
+
+<form action="?/register" method="post">
+  <fieldset>
+    <label for="username">Username</label>
+    <input type="text" id="username" name="username" />
+
+    <label for="password">Password</label>
+    <input type="password" id="password" name="password" />
+  </fieldset>
+
+  {#if form?.user}
+    <p class="form-error">Username is unavailable.</p>
+  {/if}
+  <button type="submit">Register</button>
+</form>
+
+<style>
+
+  .form-error {
+    color: red;
+  }
+
+</style>
