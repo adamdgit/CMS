d491996c9fba1ac20d6ab18f9a0756da91b86097
adamdgit <adam_d@outlook.com>
2023-06-29 21:37:20 +1000

    Added login functionality

---
+++ src/routes/login/+page.server.ts
@@ -0,0 +1,54 @@
+import { redirect, fail } from "@sveltejs/kit";
+import type { PageServerData } from "../$types";
+import bcrypt from 'bcrypt'
+import { db } from "$lib";
+import type { Action, Actions } from "@sveltejs/kit";
+
+export const load:PageServerData = async () => {
+  //
+}
+
+const login:Action = async ({ cookies, request }) => {
+  const data = await request.formData();
+  const username = data.get('username');
+  const password = data.get('password');
+
+  // simple validation
+  if (typeof username !== 'string' || typeof password !== 'string'
+  || !username || !password) {
+    return fail(400, { invalid: true })
+  }
+
+  const user = await db.users.findUnique({
+    where: { username }
+  })
+
+  if (!user) {
+    return fail(400, { credentials: true })
+  }
+
+  const passwordMatches = await bcrypt.compare(password, user.password)
+
+  if (!passwordMatches) {
+    return fail(400, { credentials: true })
+  }
+
+  // set new authentication token which also gets stored to cookies
+  const authUser = await db.users.update({
+    where: { username: user.username },
+    data: { authToken: crypto.randomUUID() }
+  })
+
+  // set auth token to cookies
+  cookies.set('session', authUser.authToken, {
+    path: '/',
+    httpOnly: true,
+    sameSite: 'strict',
+    secure: process.env.NODE_ENV === 'production',
+    maxAge: 60 * 60 * 24 * 30 // 30 days as seconds
+  })
+
+  throw redirect(302, '/account');
+}
+
+export const actions: Actions = { login };
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  import type { ActionData } from "./$types";
+
+  export let form: ActionData;
+</script>
+
+<h1>Login</h1>
+
+<form action="?/login" method="post">
+  <fieldset>
+    <label for="username">Username</label>
+    <input type="text" id="username" name="username" />
+
+    <label for="password">Password</label>
+    <input type="password" id="password" name="password" />
+  </fieldset>
+
+  {#if form?.invalid}
+    <p class="form-error">Username and password fields are required.</p>
+  {/if}
+
+  {#if form?.credentials}
+    <p class="form-error">Invalid login credentials.</p>
+  {/if}
+
+  <button type="submit">Login</button>
+</form>
+
+<style>
+
+  .form-error {
+    color: red;
+  }
+
+</style>
