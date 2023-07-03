3aca004059a90074fc817282b93f8a6da92763a7
adamdgit <adam_d@outlook.com>
2023-07-03 14:39:44 +1000

    Default header layout for all pages

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,25 @@
+<script>
+  import { page } from "$app/stores";
+</script>
+
+<svelte:head>
+  <title>CMS app</title>
+</svelte:head>
+
+<nav>
+  {#if !$page.data.user}
+    <a href="/login">Login</a>
+    <a href="/register">Register</a>
+  {/if}
+
+  {#if $page.data.user}
+    <a href="/account">Account</a>
+    <form action="/logout" method="post">
+      <button type="submit">Logout</button>
+    </form>
+  {/if}
+</nav>
+
+<main>
+  <slot />
+</main>
