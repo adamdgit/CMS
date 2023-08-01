ec136819053e7e8cc72f2d92d1a2614932784465
adamdgit <adam_d@outlook.com>
2023-08-01 16:50:22 +1000

    Added a create post function to the accounts page

--- src/routes/account/+page.server.ts
+++ src/routes/account/+page.server.ts
@@ -1,8 +1,43 @@
-import { redirect } from "@sveltejs/kit";
+import { redirect, fail} from "@sveltejs/kit";
+import type { Actions, Action } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
+import { db } from "$lib";
+
+let userId: string;
 
 export const load: PageServerLoad = async ({ locals }) => {
   if (!locals.user) {
     throw redirect(302, '/')
   }
+  userId = locals.user.id;
 }
+
+const createPost: Action = async ({ request }) => {
+  const data = await request.formData()
+  const postTitle = data.get('title')
+  const postBody = data.get('body')
+
+  // validate title
+  if (typeof postTitle !== 'string' || !postTitle) {
+    return fail(400, { postTitle: true })
+  }
+
+  // validate body
+  if (typeof postBody !== 'string' || !postBody) {
+    return fail(400, { postBody: true })
+  }
+
+  // if no errors, create post and return postid
+  const blogID = await db.posts.create({
+    data: {
+      title: postTitle,
+      content: postBody,
+      author_ref: userId
+    }, 
+    select: { id: true }
+  })
+
+  throw redirect(303, `/blog/${blogID.id}`)
+}
+
+export const actions: Actions = { createPost }
