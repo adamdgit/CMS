c0b39906c7f0ccdb2a13a50def102261c733ecfc
adamdgit <adam_d@outlook.com>
2023-08-01 10:15:57 +1000

    Testing logged in user data

---
+++ src/routes/blog/[slug]/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { page } from '$app/stores';
+
+  export let data;
+</script>
+
+<h1>{$page.status}</h1>
+
+<p>{$page.params.slug}</p>
+
+<p>{data.blogPost?.title}</p>
+<p>{data.blogPost?.content}</p>
+<p>{data.author?.username}</p>
