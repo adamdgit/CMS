8b296daffacf151609248b748ad69b6b5534bde2
adamdgit <adam_d@outlook.com>
2023-08-01 18:25:32 +1000

    Homepage now displays recent posts by all users

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,45 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import { page } from '$app/stores';
+
+  type postsType = {
+    id: string,
+    title: string,
+    content: string,
+    created_at: Date,
+    updated_at: Date,
+    likes: number,
+    author_ref: string
+  }
+
+  export let data;
+  const posts: postsType[] | undefined = data.posts;
+</script>
+
+<h1>Adams Blog</h1>
+
+<h2>Latest Posts</h2>
+
+{#if !posts}
+
+  <h3>No posts found.</h3>
+
+  {:else}
+  {#each posts as post}
+  <div class="posts-wrap">
+    <a href={`/blog/${post.id}`}>{post.title}</a>
+    <p>{post.content}</p>
+    <p>Likes: {post.likes}</p>
+    <p>Last Updated: {post.updated_at.toLocaleString("en-au", {day: '2-digit', month: 'long', year: 'numeric'})}</p>
+  </div>
+  {/each}
+
+{/if}
+
+<style>
+  .posts-wrap {
+    display: grid;
+    background-color: lightblue;
+    margin: 10px;
+    padding: 1rem;
+  }
+</style>
