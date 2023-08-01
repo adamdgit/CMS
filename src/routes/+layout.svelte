f156fad02cff0ae37f7bce0946e9dc5b4dad8fce
adamdgit <adam_d@outlook.com>
2023-08-01 10:13:50 +1000

    Added navbar with styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,25 +1,73 @@
 <script>
   import { page } from "$app/stores";
+  import '../styles.css'
+  import logo from "$lib/logo.webp"
 </script>
 
 <svelte:head>
   <title>CMS app</title>
 </svelte:head>
 
-<nav>
+<header>
+
+  <img class="logo" src={logo} alt="logo" />
+
   {#if !$page.data.user}
+  <nav>
     <a href="/login">Login</a>
     <a href="/register">Register</a>
+  </nav>
   {/if}
 
   {#if $page.data.user}
-    <a href="/account">Account</a>
+  <div class="profile">
+    <svg xmlns="http://www.w3.org/2000/svg" height="80px" fill="currentcolor" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M399 384.2C376.9 345.8 335.4 320 288 320H224c-47.4 0-88.9 25.8-111 64.2c35.2 39.2 86.2 63.8 143 63.8s107.8-24.7 143-63.8zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm256 16a72 72 0 1 0 0-144 72 72 0 1 0 0 144z"/></svg>
+    <a href="/account">{$page.data.user.name}</a>
     <form action="/logout" method="post">
-      <button type="submit">Logout</button>
+      <button class="logout" type="submit">Logout</button>
     </form>
+  </div>
   {/if}
-</nav>
+
+</header>
+
 
 <main>
   <slot />
 </main>
+
+<style>
+
+  a:visited, a {
+    color: #eee;
+  }
+
+  header {
+    display: flex;
+    justify-content: space-between;
+    background-color: #2c3135;
+    padding: 2rem;
+    color: #eee;
+  }
+
+  .logo {
+    border-radius: 50%;
+    height: 130px;
+    width: 130px;
+  }
+
+  .profile {
+    display: grid;
+    justify-items: center;
+  }
+
+  .logout {
+    padding: 10px;
+    border-radius: 5px;
+  }
+
+  .logout:hover {
+    background-color: #ccc;
+  }
+
+</style>
