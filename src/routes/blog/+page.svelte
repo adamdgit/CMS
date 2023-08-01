22d64e5e50ff72f14c67ca8fd40f4c55efb3cc65
adamdgit <adam_d@outlook.com>
2023-08-01 16:51:55 +1000

    Added blog page which displays recent blog posts

---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import { page } from '$app/stores';
+
+  type postsType = {
+    id: string,
+    title: string,
+    content: string,
+    creaated_at: Date,
+    updated_at: Date,
+    likes: number,
+    author_ref: string
+  }
+
+  export let data;
+  const posts: postsType[] = data.posts;
+</script>
+
+{#each posts as post}
+<div class="posts-wrap">
+  <p>{post.title}</p>
+  <p>{post.content}</p>
+  <p>Likes: {post.likes}</p>
+  <p>Last Updated: {post.updated_at.toLocaleString("en-au", {day: '2-digit', month: 'long', year: 'numeric'})}</p>
+</div>
+{/each}
+
+<style>
+  .posts-wrap {
+    display: grid;
+    background-color: lightblue;
+    margin: 10px;
+    padding: 1rem;
+  }
+</style>
